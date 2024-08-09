7a106aa789c3d4c65cf56782cf671765077cfd4f
fatemzh <abid.fatem@hotmail.fr>
2024-08-09 12:30:51 +0200

    form submission's working

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,50 @@
 const errMessage = document.getElementById("error-message");
 const email = document.getElementById("email-address");
-const submit = document.getElementById("submit-btn");
 const dismissBtn = document.getElementById("dismiss-btn");
 const upperContainer = document.getElementById("upper-container");
 const lowerContainer = document.getElementById("lower-container");
-const imageDesktop = document.getElementById("image-desktop");
 const confirmation = document.getElementById("confirmation");
+const form = document.getElementById("signup-form");
 
 errMessage.style.display = "none";
-upperContainer.style.display = "none";
-submit.addEventListener("click", (e) => {
-  e.preventDefault();
+confirmation.style.display = "none";
+upperContainer.style.display = "none"; 
 
+const handleSubmit = (e) => {
+  e.preventDefault();
+  
   if (email.value === "" || !email.validity.valid) {
     errMessage.style.display = "block";
   } else {
+    errMessage.style.display = "none";
+    
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData.entries()); 
+
+    console.log(data);
+
+    // Show confirmation message and hide the form
     upperContainer.style.display = "none";
     lowerContainer.style.display = "none";
     confirmation.style.display = "flex";
   }
-});
+}
+
+form.addEventListener("submit", handleSubmit);
 
 dismissBtn.addEventListener("click", (e) => {
   e.preventDefault();
   confirmation.style.display = "none";
   upperContainer.style.display = "none";
   lowerContainer.style.display = "flex";
-});
+
+  if (window.innerWidth <= 1440) {
+    upperContainer.style.display ="flex";
+  }
+  });
+
 
 if (window.innerWidth <= 1440) {
   confirmation.style.display = "none";
   upperContainer.style.display = "flex";
-
-  dismissBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    confirmation.style.display = "none";
-    upperContainer.style.display = "flex";
-  });
 }
